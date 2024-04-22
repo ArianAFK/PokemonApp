5a37413a25ce24b7e4ce645f91a42baefa654cc5
ArianAFK <arianfekri@hotmail.com>
2024-04-22 14:44:40 +0200

    fetch from api

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,7 @@
+const Card = ({ colorChoice, children }: { colorChoice: string, children: React.ReactNode }) => {
+    return <div className={colorChoice}>
+        {children}
+    </div>
+}
+
+export default Card;
